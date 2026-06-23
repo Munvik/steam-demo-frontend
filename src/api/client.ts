f2262594329823ae788b039df6141e817d953278
Monfikowsky <bartek.mikolaj.wloszek@wp.pl>
2026-06-23 14:37:34 +0200

    Update envs

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'https://localhost:7226',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
